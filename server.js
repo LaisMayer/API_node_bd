8043992d78c6e585d202d4245210969284b50423
Lais Mayer <20233007175@estudantes.ifpr.edu.br>
2025-08-19 16:58:48 -0300

    okk

--- server.js
+++ server.js
@@ -1,10 +1,5 @@
-import dataRoutes from './router/router.js';
 import express from 'express';
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-const _filename = fileURLToPath(import.meta.url);
-const _dirname = path.dirname(_filename);
+import dataRoutes from './router/router.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -14,7 +9,7 @@ app.use(express.json());
 app.use('/api', dataRoutes);
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(_dirname, 'public', 'index.html'));
+  res.json({ message: 'Funciona :)' });
 });
 
 app.listen(PORT, () => {
